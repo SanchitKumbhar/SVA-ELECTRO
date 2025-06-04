127c40eb5c5916b870e0fd1d45ea766d0f30ec4c
Sanchit Kumbhar <sanchitskumbhar@gmail.com>
2025-06-04 11:46:12 +0530

    order apis made with docs uploaded

---
+++ Backend/admin/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from products.models import Product
+
+
+class ProductModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields="__all__"
+    
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.productname=validated_data.get("productname",instance.productname)
+        instance.productcategory=validated_data.get("productcategory",instance.productcategory) 
+        instance.stock=validated_data.get("stock",instance.stock)
+        instance.productimg=validated_data.get("productimg",instance.productimg)
+        instance.cost=validated_data.get("cost",instance.cost)
+        instance.description=validated_data.get("description",instance.description)     
+        instance.launch=validated_data.get("launch",instance.launch)
+        instance.save()
+        return instance
+
+    
+    
--- Backend/admin/urls.py
+++ Backend/admin/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewclass  # Import your viewset explicitly
+from .views import Productview  # Import your viewset explicitly
 
 router = DefaultRouter()
-router.register(r'order', OrderViewclass, basename='order')
+router.register(r'products', Productview, basename='create-product')  
 
 urlpatterns = [
     path('', include(router.urls)),
