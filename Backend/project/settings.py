1e66505e3acd6103b6e7266d02dc30d364d2ed31
Sanchit <sanchitskumbhar@gmail.com>
2025-06-08 15:45:17 +0530

    cors

--- Backend/project/settings.py
+++ Backend/project/settings.py
@@ -45,11 +45,13 @@ INSTALLED_APPS = [
     "productsapi",
     "appointment",
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+        'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",   # for local dev
+    # "https://yourfrontend.com",  # your live frontend
+]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'project.urls'
